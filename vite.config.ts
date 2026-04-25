e67aea051ab70b4240f7a9ef216c7dbf41f8623c
emsi1309 <congnbm1309@gmail.com>
2026-04-25 20:53:57 +0700

    Refactor sitemap generation settings and adjust Vue plugin import syntax

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-import * as vue from "@vitejs/plugin-vue";
+// @ts-ignore
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
